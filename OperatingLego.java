bd738061b9c2b87f0f01c6d1ac7a28737e67f672
colonee <colonee11@gmail.com>
2016-07-08 16:43:23 +0900

    ひな型追加
    OperatingLego.javaを追加しました。

---
+++ OperatingLego.java
@@ -0,0 +1,64 @@
+package legomindstorm;
+
+public class OperatingLego {
+	public static void main(String[] args){
+		
+	}
+	void main(){
+		init();			//初期化処理
+		while(!isGoal()){	//ゴールにつくまで処理を回す
+			processCommand();
+		}
+		del();		//終了処理
+		
+	}
+	//
+	void init(){
+		
+	}
+	
+	void startCommandProcessing(){
+		
+	}
+	void finishCommandProcessing(){
+		
+	}
+	void Processcommand(){
+		
+	}
+	void increaseLeftWheelSpeed(){
+		
+	}
+	void decreaseLeftWheelSpeed(){
+		
+	}
+	void increaseRightWheelSpeed(){
+		
+	}
+	void decreaseRightWheelSpeed(){
+		
+	}
+	void avoidKeepOutArea(){
+		
+	}
+	void stopCar(){
+		
+	}
+	//ゴールしてるかどうか？
+	boolean isGoal(){
+		return null;
+	}
+	
+	
+	void RunCar(){
+		
+	}
+	//車輪の速度を初期化する
+	void initializeCarWheel(){
+		
+	}
+	//処理を終了する
+	void del(){
+		
+	}
+}
